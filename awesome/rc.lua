360519f7f1835d46e836478f02ff4c022eb54c9f
Tom Okas <git@tomokas.com>
2012-10-30 10:45:56 +0000

    Volume keys working.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -252,7 +252,12 @@ globalkeys = awful.util.table.join(
                   mypromptbox[mouse.screen].widget,
                   awful.util.eval, nil,
                   awful.util.getdir("cache") .. "/history_eval")
-              end)
+              end),
+
+    -- Volume
+    awful.key({}, "XF86AudioLowerVolume", function () awful.util.spawn("amixer -q sset Master 5%-") end),
+    awful.key({}, "XF86AudioRaiseVolume", function () awful.util.spawn("amixer -q sset Master 5%+") end),
+    awful.key({}, "XF86AudioMute", function () awful.util.spawn("amixer -q sset Master toggle") end)
 )
 
 clientkeys = awful.util.table.join(
